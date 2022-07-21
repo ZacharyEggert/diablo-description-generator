5aa8ad1393a7b3d8e79422949635b4bc1c09d554
Zachary Eggert <Eggert.Zachary@gmail.com>
2022-07-21 14:04:56 -0700

    acoustics

--- components/Form/AcousticForm.tsx
+++ components/Form/AcousticForm.tsx
@@ -21,8 +21,44 @@ const AcousticForm: React.FC<{
 
   const makeFormSelect = (label: string, key: keyof IAcousticFormContext) => {
     let opt = [];
-
-    opt = acousticFormOptions[key];
+    if (key === 'acousticModel') {
+      if (
+        currentOptions.acousticMake &&
+        currentOptions.acousticMake !== 'sel'
+      ) {
+        opt = acousticFormOptions.acousticModel[currentOptions.acousticMake];
+      } else {
+        opt = [];
+      }
+    } else if (key === 'acousticSubModel') {
+      if (
+        currentOptions.acousticModel &&
+        currentOptions.acousticMake &&
+        currentOptions.acousticMake !== 'sel' &&
+        currentOptions.acousticModel !== 'sel'
+      ) {
+        opt =
+          acousticFormOptions.acousticSubModel[currentOptions.acousticMake][
+            currentOptions.acousticModel
+          ];
+      } else {
+        opt = [];
+      }
+    } else if (key === 'acousticTuningMachineModels') {
+      if (
+        currentOptions.acousticTuningMachineBrands &&
+        currentOptions.acousticTuningMachineBrands !== 'sel'
+      ) {
+        opt =
+          acousticFormOptions.acousticTuningMachineModels[
+            currentOptions.acousticTuningMachineBrands
+          ];
+      } else {
+        opt = [];
+      }
+    } else {
+      opt = acousticFormOptions[key] as string[];
+    }
 
     return (
       <FormSelect
@@ -37,6 +73,16 @@ const AcousticForm: React.FC<{
     );
   };
 
+  /**
+   *
+   * @param key key of IFormContext to generate a selection for.
+   * @returns a FormSelect JSX element.
+   * @description generates a FormSelect element for the given key.
+   */
+  const MFST = (key: keyof IAcousticFormContext) => {
+    return makeFormSelect(acousticFormLabelPrettier[key], key);
+  };
+
   return (
     <div className='flex w-full flex-col px-12 pt-12 text-white'>
       <form
@@ -47,6 +93,14 @@ const AcousticForm: React.FC<{
           <h2 className='text-center text-2xl font-semibold text-white lg:col-span-2'>
             General
           </h2>
+          {MFST('acousticMake')}
+          {MFST('acousticModel')}
+          {MFST('acousticSubModel')}
+          {MFST('acousticCountryOfOrigin')}
+          {MFST('acousticYear')}
+          {MFST('acousticFinish')}
+          {MFST('acousticSerial')}
+          {MFST('acousticCondition')}
         </div>
 
         <div className='col-span-2 flex min-h-[3rem] w-full flex-col lg:col-span-3'>
@@ -55,12 +109,30 @@ const AcousticForm: React.FC<{
             <h2 className='text-center text-2xl font-semibold text-white lg:col-span-1'>
               Neck
             </h2>
+            {MFST('acousticFingerBoard')}
+            {MFST('acousticRadius')}
+            {MFST('acousticNeckFinish')}
+            {MFST('acousticNeckWood')}
+            {MFST('acousticNeckProfile')}
+            {MFST('acousticNeckThickness_1')}
+            {MFST('acousticNeckThickness_12')}
+            {MFST('acousticFrets')}
+            {MFST('acousticScaleLength')}
+            {MFST('acousticNeckJoint')}
+            {MFST('acousticNut')}
+            {MFST('acousticNutWidth')}
+            {MFST('acousticInlays')}
           </div>
           {/*Body*/}
           <div className='mb-4 grid flex-none grid-cols-1 rounded-2xl bg-neutral-500 py-2 shadow-md shadow-neutral-900'>
             <h2 className='text-center text-2xl font-semibold text-white lg:col-span-1'>
               Body
             </h2>
+            {MFST('acousticBackWood')}
+            {MFST('acousticSideWood')}
+            {MFST('acousticTopWood')}
+            {MFST('acousticFinishType')}
+            {MFST('acousticBodyShape')}
           </div>
         </div>
 
@@ -70,12 +142,21 @@ const AcousticForm: React.FC<{
             <h2 className='text-center text-2xl font-semibold text-white lg:col-span-1'>
               Hardware
             </h2>
+            {MFST('acousticBridge')}
+            {MFST('acousticPickguard')}
+            {MFST('acousticStrapButtons')}
+            {MFST('acousticStringGauge')}
+            {MFST('acousticTuningMachineBrands')}
+            {MFST('acousticTuningMachineModels')}
+            {MFST('acousticTuningMachineHeads')}
           </div>
           {/*Electronics*/}
           <div className='mb-4 grid flex-none grid-cols-1 rounded-2xl bg-neutral-500 py-2 shadow-md shadow-neutral-900'>
             <h2 className='text-center text-2xl font-semibold text-white lg:col-span-1'>
               Electronics
             </h2>
+            {MFST('acousticElectronics')}
+            {MFST('acousticElectronicsType')}
           </div>
         </div>
         {/*Misc Section*/}
@@ -83,6 +164,10 @@ const AcousticForm: React.FC<{
           <h2 className='text-center text-2xl font-semibold text-white lg:col-span-2'>
             Miscellaneous
           </h2>
+          {MFST('acousticWeight')}
+          {MFST('acousticModifications')}
+          {MFST('acousticOtherFeatures')}
+          {MFST('acousticGuitarCase')}
         </div>
 
         {/* {Object.keys(formOptions).map((key) => {
@@ -92,7 +177,7 @@ const AcousticForm: React.FC<{
                             currentOptions.make &&
                             currentOptions.make !== 'sel'
                         ) {
-                            opt = formOptions.model[currentOptions.make];
+                            opt = acousticFormOptions.model[currentOptions.make];
                         } else {
                             opt = [];
                         }
@@ -104,7 +189,7 @@ const AcousticForm: React.FC<{
                             currentOptions.model !== 'sel'
                         ) {
                             opt =
-                                formOptions.subModel[currentOptions.make][
+                                acousticFormOptions.subModel[currentOptions.make][
                                     currentOptions.model
                                 ];
                         } else {
@@ -116,14 +201,14 @@ const AcousticForm: React.FC<{
                             currentOptions.tuningMachineBrands !== 'sel'
                         ) {
                             opt =
-                                formOptions.tuningMachineModels[
+                                acousticFormOptions.tuningMachineModels[
                                     currentOptions.tuningMachineBrands
                                 ];
                         } else {
                             opt = [];
                         }
                     } else {
-                        opt = formOptions[key];
+                        opt = acousticFormOptions[key];
                     }
 
                     if (
